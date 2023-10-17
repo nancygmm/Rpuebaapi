0c8319a27af497387f76db99fdd71c55eaf71b57
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-10-17 08:38:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/rpuebaapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rpuebaapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rpuebaapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
